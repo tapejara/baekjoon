e3254e03604436f06a2530343be3ed358f9c0d29
tapejara <wolfrink444@gmail.com>
2024-11-04 14:39:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -3,6 +3,5 @@ for _ in range(a):
     b,c = map(str,input().split())
     string = ""
     for element in c:
-        for i in range(int(b)):
-             string += element
-    print(string)    
+        string += element*int(b)
+    print(string)
