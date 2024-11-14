c53c7fa84202318007961b9f885a2739f24d1636
tapejara <wolfrink444@gmail.com>
2024-11-14 18:29:48 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -2,17 +2,11 @@ b,a = map(int,input().split())
 list1 = []
 list2 = []
 for _ in range(b):
-    try:
-        c = list(map(int,input().split()))
-        list1.append(c)
-    except EOFError:
-        break
+    c = list(map(int,input().split()))
+    list1.append(c)
 for _ in range(b):
-    try:
-        c = list(map(int,input().split()))
-        list2.append(c)
-    except EOFError:
-        break
+    c = list(map(int,input().split()))
+    list2.append(c)
 for i in range(b):
     for j in range(a):
         if(j == a-1):
