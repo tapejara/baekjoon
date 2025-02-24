9c79e372ae26292b709979c7ef044f93bdb39582
tapejara <wolfrink444@gmail.com>
2025-02-24 17:04:06 +0900

    [Silver III] Title: 돌 게임 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -1,13 +1,12 @@
 n = int(input())
-list1 = [0, 1, 2, 1, 1, 3]
-for i in range(6, n + 1):
-    if(list1[i - 4] % 2 == 0):
-        list1.append(list1[i - 4] + list1[4])
-    elif(list1[i - 3] % 2 == 0):
-        list1.append(list1[i - 3] + list1[3])
+list1 = ["SK", "CY", "SK", "SK"]
+for i in range(4, n):
+    if(list1[i - 4] == "CY"):
+        list1.append("SK")
+    elif(list1[i - 3] == "CY"):
+        list1.append("SK")
+    elif(list1[i - 1] == "CY"):
+        list1.append("SK")
     else:
-        list1.append(list1[i - 1] + list1[1])
-if(list1[n] % 2 > 0):
-    print("SK")
-else:
-    print("CY")
+        list1.append("CY")
+print(list1[n - 1])
