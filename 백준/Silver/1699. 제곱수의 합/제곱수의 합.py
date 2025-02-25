07e8642f5b27d9704dcb8e2e5078dd2acda9101e
tapejara <wolfrink444@gmail.com>
2025-02-25 15:54:13 +0900

    [Silver II] Title: 제곱수의 합, Time: 6368 ms, Memory: 36308 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -4,13 +4,10 @@ i = 2
 while list1[-1] < n:
     list1.append(i ** 2)
     i += 1
-list2 = [[], [1]]
+dp = [i for i in range(n + 1)]
 for j in range(2, n + 1):
-    list3 = []
     for element in list1:
-        if(j < element):
+        if(element > j):
             break
-        list3.append(list2[j - element] + [element])
-    list3.sort(key = len)
-    list2.append(list3[0])
-print(len(list2[n]))
+        dp[j] = min(dp[j - element] + 1, dp[j])
+print(dp[n])
