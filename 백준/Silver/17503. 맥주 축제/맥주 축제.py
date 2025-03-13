6fa8f4f7a7dc60755448539acc63e63baf471ab2
tapejara <wolfrink444@gmail.com>
2025-03-13 21:48:14 +0900

    [Silver I] Title: 맥주 축제, Time: 496 ms, Memory: 65584 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -1,28 +1,17 @@
 import sys, heapq
 input = sys.stdin.readline
 n, m, k = map(int,input().split())
-beer = []
-vi = []
-level = 0
-for _ in range(k):
-    v, c = map(int,input().split())
-    heapq.heappush(beer,(c, v))
-for _ in range(n):
-    pop1 =  heapq.heappop(beer)
-    heapq.heappush(vi,pop1[1])
-    m -= pop1[1]
-    level = pop1[0]
-if(m > 0):
-    while m > 0 and beer:
-        pop2 = heapq.heappop(vi)
-        pop3 = heapq.heappop(beer)
-        heapq.heappush(vi,pop3[1])
-        m += pop2
-        m -= pop3[1]
-        level = pop3[0]
-    if(m > 0):
-        print(-1)
-    else:
-        print(level)
-else:
-    print(level)
+beer = list(tuple(map(int,input().split())) for _ in range(k))
+beer.sort(key = lambda x: x[1])
+hq = []
+total = 0
+level = -1
+for i in range(len(beer)):
+    heapq.heappush(hq, beer[i][0])
+    total += beer[i][0]
+    if(len(hq) == n and total < m):
+        total -= heapq.heappop(hq)
+    elif(len(hq) == n and total >= m):
+        level = beer[i][1]
+        break
+print(level)
