65ce6fb6f48ed28bbd5e3b1e9d2192fadb838129
tapejara <wolfrink444@gmail.com>
2025-04-03 20:19:39 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 1024 ms, Memory: 37288 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
@@ -2,21 +2,18 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 n, m, a, b = map(int,input().split())
-dog = [0 for _ in range(n + 1)]
+dog = [-1 for _ in range(n + 1)]
 for _ in range(m):
     l, r = map(int,input().split())
     for i in range(l, r + 1):
-        dog[i] = 1
+        dog[i] = 0
 q = deque([(0, 0)])
 while q:
     c, count = q.popleft()
     if(c == n):
-        print(count)
-        exit()
-    if(c + a <= n and dog[c + a] != 1):
-        q.append((c + a, count + 1))
-        dog[c + a] = 1
-    if(c + b <= n and dog[c + b] != 1):
-        q.append((c + b, count + 1))
-        dog[c + b] = 1
-print(-1)
+        break
+    for i in [a, b]:
+        if(c + i <= n and dog[c + i] == -1):
+            q.append((c + i, count + 1))
+            dog[c + i] = count + 1
+print(dog[-1])
