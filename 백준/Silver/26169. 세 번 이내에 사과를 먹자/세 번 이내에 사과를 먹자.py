1fea105d6875a9a1f13e093703dcc622d1ba2d39
tapejara <wolfrink444@gmail.com>
2025-04-10 21:04:59 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
@@ -1,25 +1,15 @@
-import sys
-input = sys.stdin.readline
 board = [list(map(int,input().split())) for _ in range(5)]
 visited = [[0 for _ in range(5)] for _ in range(5)]
 r, c = map(int,input().split())
-flag = board[r][c]
-apple = board[r][c]
-distance = 0
-def dfs(y, x, d):
-    global apple
+visited[r][c] = -1
+def dfs(y, x, d, apple):
     if(apple >= 2):
         print(1)
         exit()
-    visited[y][x] = -1
     for a,b in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
         if(-1 < y + a < 5 and -1 < x + b < 5 and board[y + a][x + b] != -1 and d < 3 and visited[y + a][x + b] != -1):
-            if(apple >= 1 and y == r and x ==c):
-                apple = flag
-            if(board[y + a][x + b] == 1):
-                apple += 1
             visited[y + a][x + b] = -1
-            dfs(y + a, x + b, d + 1)
+            dfs(y + a, x + b, d + 1, apple + board[y + a][x + b])
             visited[y + a][x + b] = 0
     return 0
-print(dfs(r, c, distance))
+print(dfs(r, c, 0, board[r][c]))
