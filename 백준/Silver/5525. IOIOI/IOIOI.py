128b2bc09b182206cefed2c2eb2b6ef7355e7443
tapejara <wolfrink444@gmail.com>
2025-04-27 22:54:35 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 49792 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,15 +1,15 @@
 n = int(input())
 m = int(input())
 s = list(input())
-c = s[0]
+c = [s[0]]
 answer = 0
 for i in range(1,m):
     if(c[-1] == "O" and s[i] == "I"):
-        c += s[i]
+        c.append(s[i])
         if(len(c) >= 2 * n + 1):
             answer += 1
     elif(c[-1] == "I" and s[i] == "O"):
-        c += s[i]
+        c.append(s[i])
     else:
-        c = s[i]
+        c = [s[i]]
 print(answer)
