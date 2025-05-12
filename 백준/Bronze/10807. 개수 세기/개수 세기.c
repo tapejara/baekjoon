7330531d67f41177620651645f053fd3f63121e8
tapejara <wolfrink444@gmail.com>
2025-05-13 00:39:45 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.c
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    scanf("%d", &i);
-    int arr[i];
-    for(int k = 0; k < i; k++) {
-        scanf("%d", &arr[k]);
-    }
-    int j;
-    scanf("%d", &j);
-    int k;
     int count = 0;
-    for(k = 0; k < i; k++) {
-        if(arr[k] == j) {
-            count++;
-        }
-    }
+    int n,v,i;
+    
+    scanf("%d",&n);
+    int arr[100] = {};
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    };
+    scanf("%d", &v);
+    for(i = 0; i < n; i++) {
+        if(arr[i] == v) {
+            count ++;
+        };
+    };
     printf("%d", count);
 }
