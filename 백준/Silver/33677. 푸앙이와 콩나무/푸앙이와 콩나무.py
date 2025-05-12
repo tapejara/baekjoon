b2f5a80874282d5c63f31180c4bab8f672238446
tapejara <wolfrink444@gmail.com>
2025-05-12 22:15:28 +0900

    [Silver I] Title: 푸앙이와 콩나무, Time: 1068 ms, Memory: 105172 KB -BaekjoonHub

---
+++ 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
@@ -0,0 +1,17 @@
+n = int(input())
+tree = [(1000000, 1000000) for _ in range(n + 1)]
+tree[0] = (0, 0)
+for i in range(n + 1):
+    if(i + 1 < n + 1 and tree[i + 1][0] > tree[i][0] + 1):
+        tree[i + 1] = (tree[i][0] + 1, tree[i][1] + 1)
+    elif(i + 1 < n + 1 and tree[i + 1][0] == tree[i][0] + 1):
+        tree[i + 1] = (tree[i][0] + 1, min(tree[i + 1][1], tree[i][1] + 1))
+    if(i * 3 < n + 1 and tree[i * 3][0] > tree[i][0] + 1):
+        tree[i * 3] = (tree[i][0] + 1, tree[i][1] + 3)
+    elif(i * 3 < n + 1 and tree[i * 3][0] == tree[i][0] + 1):
+        tree[i * 3] = (tree[i][0] + 1, min(tree[i * 3][1], tree[i][1] + 3))    
+    if(i ** 2 < n + 1 and tree[i ** 2][0] > tree[i][0] + 1):
+        tree[i ** 2] = (tree[i][0] + 1, tree[i][1] + 5)
+    elif(i ** 2 < n + 1 and tree[i ** 2][0] == tree[i][0] + 1):
+        tree[i ** 2] = (tree[i][0] + 1, min(tree[i ** 2][1], tree[i][1] + 5))
+print(tree[n][0], tree[n][1])
