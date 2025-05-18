26f338056383ce085144b06592872ab815a3dfc7
tapejara <wolfrink444@gmail.com>
2025-05-18 23:56:07 +0900

    [Bronze IV] Title: 나이 계산하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16199. 나이 계산하기/나이 계산하기.py
@@ -0,0 +1,10 @@
+y1, m1, d1 = map(int,input().split())
+y2, m2, d2 = map(int,input().split())
+if(m1 < m2):
+    print(y2 - y1)
+elif(m1 == m2 and d1 <= d2):
+    print(y2 - y1)
+else:
+    print(y2 - y1 - 1)
+print(y2 - y1 + 1)
+print(y2 - y1)
