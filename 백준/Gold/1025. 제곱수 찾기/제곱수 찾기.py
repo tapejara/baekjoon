7eec5f884cccc8434f619c0fd0ca978c8fe3eef2
tapejara <wolfrink444@gmail.com>
2025-05-26 00:05:27 +0900

    [Gold V] Title: 제곱수 찾기, Time: 84 ms, Memory: 36000 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
@@ -1,7 +1,5 @@
-list1 = []
+set1 = set([i ** 2 for i in range(31623)])
 answer = -1
-for i in range(31623):
-    list1.append(i ** 2)
 n, m = map(int,input().split())
 board = [[i for i in input()] for _ in range(n)]
 list2 = []
@@ -14,14 +12,14 @@ for i in range(n):
             x = j
             y = i
             temp = board[i][j]
-            if(int(temp) in list1):
+            if(int(temp) in set1):
                 answer = max(int(temp), answer)
             for k in range(max(n,m) - 1):
                 if(-1 < x + a < m and -1 < y + b < n):
                     x += a
                     y += b
                     temp += board[y][x]
-                    if(int(temp) in list1):
+                    if(int(temp) in set1):
                         answer = max(int(temp), answer)
                 else:
                     continue
