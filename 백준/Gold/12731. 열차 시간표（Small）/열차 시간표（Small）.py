6983fbd93bc8c703d9f638126df4391591d1eb9c
tapejara <wolfrink444@gmail.com>
2025-07-28 17:33:51 +0900

    [Gold V] Title: 열차 시간표(Small), Time: 56 ms, Memory: 36640 KB -BaekjoonHub

---
+++ 백준/Gold/12731. 열차 시간표（Small）/열차 시간표（Small）.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+import heapq
+input = sys.stdin.readline
+n = int(input())
+for x in range(n):
+    t = int(input())
+    na, nb = map(int,input().split())
+    list1 = []
+    for _ in range(na):
+        a, b = map(str,input().split())
+        c, d = map(int,a.split(":"))
+        e, f = map(int,b.split(":"))
+        start = c * 60 + d
+        end = e * 60 + f + t
+        list1.append([start,end,0])
+    for _ in range(nb):
+        a, b = map(str,input().split())
+        c, d = map(int,a.split(":"))
+        e, f = map(int,b.split(":"))
+        start = c * 60 + d
+        end = e * 60 + f + t
+        list1.append([start,end,1])
+    answer_a = na
+    answer_b = nb
+    list1.sort()
+    dq = deque(list1.copy())
+    while dq:
+        temp = dq.popleft()
+        for i in range(len(list1)):
+            if(temp[1] <= list1[i][0] and temp[2] != list1[i][2]):
+                if(list1[i][2] == 0):
+                    answer_a -= 1
+                else:
+                    answer_b -= 1
+                list1.remove(list1[i])
+                break
+    print(f'Case #{x + 1}: {answer_a} {answer_b}')
