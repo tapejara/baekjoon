72242dd18dc4cf015ebede27677db641446eb1cb
tapejara <wolfrink444@gmail.com>
2025-08-07 14:28:11 +0900

    [Silver IV] Title: 마작에서 가장 어려운 것, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/33049. 마작에서 가장 어려운 것/마작에서 가장 어려운 것.py
+++ 백준/Silver/33049. 마작에서 가장 어려운 것/마작에서 가장 어려운 것.py
@@ -1,8 +1,6 @@
 p3, p4, p0 = map(int,input().split())
-count3 = 0
-count4 = 0
-count3 += p3 // 3
-count4 += p4 // 4
+count3 = p3 // 3
+count4 = p4 // 4
 p3 %= 3
 p4 %= 4
 if(p3 > 0):
@@ -11,25 +9,10 @@ if(p3 > 0):
 if(p4 > 0):
     p0 -= 4 - p4
     count4 += 1
-if(p0 < 0):
-    print(-1)
-    exit()
-temp = 0
-temp += p0 // 4
-p0 %= 4
-if(p0 == 3):
-    print( count3 + 1, count4 + temp)
-elif(p0 == 2):
-    temp -= 1
-    if(temp < 0):
-        print(-1)
-    else:
-        print(count3 + 2, count4 + temp)
-elif(p0 == 1):
-    temp -= 2
-    if(temp < 0):
-        print(-1)
-    else:
-        print(count3 + 3, count4 + temp)
-else:
-    print(count3, count4 + temp)
+for i in range(4):
+    temp = p0
+    temp -= 3 * i
+    if(temp >= 0 and temp % 4 == 0):
+        print(count3 + i, count4 + temp // 4)
+        exit()
+print(-1)
