f81ce0cc359ac31eedcabc8103e9fca1821b3337
tapejara <wolfrink444@gmail.com>
2025-08-12 15:37:02 +0900

    [Silver III] Title: 다음 순열, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,21 @@
+n = int(input())
+list1 = list(map(int,input().split()))
+flag = True
+for i in range(n - 1, 0, -1):
+    if(list1[i] > list1[i - 1]):
+        flag = False
+        check = list1[i - 1]
+        list1[i - 1] = max(list1[i::])
+        for j in range(n -1, i - 1, -1):
+            if(list1[i - 1] >= list1[j] and check < list1[j]):
+                list1[i - 1], list1[j] = list1[j], check
+        list2 = list1[:i]
+        list3 = list1[i:]
+        list3.sort()
+        break
+if(flag == True):
+    print(-1)
+else:
+    answer = list2 + list3
+    for i in range(n):
+        print(answer[i], end=" ")
