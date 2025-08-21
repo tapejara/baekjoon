6e156027052d00bd7ae215dc81afbf8e85912d8c
tapejara <wolfrink444@gmail.com>
2025-08-21 14:48:45 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 204 ms, Memory: 50140 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 import sys
 input = sys.stdin.readline
 n, m = map(int,input().split())
@@ -5,20 +6,8 @@ list1 = []
 list2 = []
 for _ in range(n):
     a, b = input().split()
-    if(list1):
-        if(list2[-1] < int(b)):
-            list1.append(a)
-            list2.append(int(b))
-    else:
-        list1.append(a)
-        list2.append(int(b))
+    list1.append(a)
+    list2.append(int(b))
 list3 = [int(input()) for _ in range(m)]
-for element in list3:
-    minimum = 0
-    maximum = len(list2) - 1
-    while minimum <= maximum:
-        mid = (minimum + maximum) // 2
-        if(element <= list2[mid]):
-            maximum = mid - 1
-        else: minimum = mid + 1
-    print(list1[maximum +1])
+for i in range(m):
+    print(list1[bisect_left(list2, list3[i])])
