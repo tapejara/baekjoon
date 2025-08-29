902f74e1460944dc6658de6ddf3886a8dbc5e3a3
tapejara <wolfrink444@gmail.com>
2025-08-29 15:12:34 +0900

    [Silver II] Title: 안녕, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,27 +1,11 @@
-import itertools
 n = int(input())
-list1 = [[] for _ in range(n)]
-input_1 = list(map(int,input().split()))
-for i in range(n):
-    list1[i].append(input_1[i])
-input_2 = list(map(int,input().split()))
-for i in range(n):
-    list1[i].append(input_2[i])
-list2 = []
-for i in range(1,n + 1):
-    temp = list(itertools.combinations(list1, i))
-    for element in temp:
-        list2.append(element)
-list3 = []
-for i in range(len(list2)):
-    a = 0
-    b = 0
-    for j in range(len(list2[i])):
-        a += list2[i][j][0]
-        b += list2[i][j][1]
-    if(a < 100):
-        list3.append(b)
-if(list3):
-    print(max(list3))
-else:
-    print(0)
+list1 = [0] + list(map(int,input().split()))
+list2 = [0] + list(map(int,input().split()))
+dp = [[0 for _ in range(100)] for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1,100):
+        if(j - list1[i] >= 0):
+            dp[i][j] = max(list2[i] + dp[i - 1][j - list1[i]], dp[i - 1][j])
+        else:
+            dp[i][j] = dp[i - 1][j]
+print(dp[n][99])
