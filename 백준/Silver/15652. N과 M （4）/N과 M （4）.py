f5692ed33a042c7ec05e0bf43ba3fc4a590dbb4d
tapejara <wolfrink444@gmail.com>
2025-09-17 12:52:21 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+list1 = [i for i in range(1, n + 1)]
+list2 = []
+def function(count, arr1, arr2):
+    if(count == m + 1):
+        for i in range(len(arr2)):
+            if(i == len(arr2) - 1):
+                print(arr2[i])
+            else:
+                print(arr2[i], end= " ")
+    else:
+        for i in range(len(arr1)):
+            list3 = arr2.copy()
+            list4 = arr1[i:]
+            list3.append(arr1[i])
+            function(count + 1, list4, list3)
+        
+function(1, list1, list2)
