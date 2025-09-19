6ef9218c3c4c0ee6070b41dfbdc5404920cc528a
tapejara <wolfrink444@gmail.com>
2025-09-19 14:03:46 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,18 @@
+n, m = map(int,input().split())
+list1 = list(map(int,input().split()))
+list1.sort()
+list1 += [0]
+list2 = []
+def function(count, arr1, arr2):
+    if(count == m + 1):
+        for i in range(len(arr2) - 1):
+            print(arr2[i], end = " ")
+        print(arr2[-1])
+    else:
+        for i in range(len(arr1) - 1):
+            if(arr1[i] != arr1[i - 1]):
+                list3 = arr1[i:]
+                list4 = arr2.copy()
+                list4.append(arr1[i])
+                function(count + 1, list3, list4)
+function(1, list1, list2)
