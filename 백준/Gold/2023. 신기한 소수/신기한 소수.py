02fc85c9cc50c73cff7595e7a136db617d0b26d1
tapejara <wolfrink444@gmail.com>
2025-09-28 17:30:34 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,20 +1,20 @@
 n = int(input())
-start = ["1","2","3","4","5","6","7","8","9"]
+start = [1, 3, 5, 7, 9]
 answer = []
-def function(count, arr1, num):
-    if(count > n):
-        answer.append(int(num))
+def function(num):
+    if(len(str(num)) == n):
+        answer.append(num)
     else:
-        for i in range(len(arr1)):
-            temp = num + arr1[i]
+        for i in range(len(start)):
+            temp = num * 10 + start[i]
             flag = True
-            for j in range(2,int(temp) // 2):
-                if(int(temp) % j == 0):
+            for j in range(2, int(temp**0.5) + 1):
+                if(temp % j == 0):
                     flag = False
                     break
             if flag:
-                function(count + 1, arr1, temp)
-for i in ["2","3","5","7"]:
-    function(2, start, i)
+                function(temp)
+for i in [2, 3, 5, 7]:
+    function(i)
 for element in answer:
     print(element)
