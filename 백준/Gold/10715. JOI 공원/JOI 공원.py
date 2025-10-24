4a64fa61485d3e4c0cee327853f3dc72d278f00b
tapejara <wolfrink444@gmail.com>
2025-10-24 17:22:16 +0900

    [Gold I] Title: JOI 공원, Time: 48 ms, Memory: 35508 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/10715. JOI 공원/JOI 공원.py
@@ -0,0 +1,32 @@
+import heapq, sys
+input = sys.stdin.readline
+n, m, c = map(int,input().split())
+list1 = [[] for _ in range(n + 1)]
+list2 = []
+total = 0
+for i in range(m):
+    a, b, d = map(int,input().split())
+    total += d
+    list1[a].append((b, d))
+    list1[b].append((a, d))
+dist_list = [1e99 for _ in range(n + 1)]
+dist_list[1] = 0
+q1 = [(0, 1)]
+cost = total
+answer = []
+while q1:
+    dist, point = heapq.heappop(q1)
+    if(dist > dist_list[point]):
+        continue
+    if(point not in list2):
+        list2.append(point)
+        temp = 0
+        for next_p, next_d in list1[point]:
+            if(dist_list[next_p] > dist_list[point] + next_d):
+                dist_list[next_p] = dist_list[point] + next_d
+                heapq.heappush(q1, (dist_list[next_p], next_p))
+            if(next_p in list2):
+                temp += next_d
+        cost -= temp
+        answer.append(cost + c * dist_list[point])
+print(min(answer))
