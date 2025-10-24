bf09a0916f9da73491024864bc38267406865e4f
tapejara <wolfrink444@gmail.com>
2025-10-24 17:55:18 +0900

    [Gold I] Title: JOI 공원, Time: 1144 ms, Memory: 104668 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10715. JOI 공원/JOI 공원.py
+++ 백준/Gold/10715. JOI 공원/JOI 공원.py
@@ -2,7 +2,7 @@ import heapq, sys
 input = sys.stdin.readline
 n, m, c = map(int,input().split())
 list1 = [[] for _ in range(n + 1)]
-list2 = []
+set1 = set()
 total = 0
 for i in range(m):
     a, b, d = map(int,input().split())
@@ -13,20 +13,22 @@ dist_list = [1e99 for _ in range(n + 1)]
 dist_list[1] = 0
 q1 = [(0, 1)]
 cost = total
-answer = []
+answer = 1e99
 while q1:
     dist, point = heapq.heappop(q1)
     if(dist > dist_list[point]):
         continue
-    if(point not in list2):
-        list2.append(point)
+    if(point not in set1):
+        set1.add(point)
         temp = 0
         for next_p, next_d in list1[point]:
             if(dist_list[next_p] > dist_list[point] + next_d):
                 dist_list[next_p] = dist_list[point] + next_d
                 heapq.heappush(q1, (dist_list[next_p], next_p))
-            if(next_p in list2):
+            if(next_p in set1):
                 temp += next_d
         cost -= temp
-        answer.append(cost + c * dist_list[point])
-print(min(answer))
+        answer = min(answer, cost + c * dist_list[point])
+    else:
+        continue
+print(answer)
