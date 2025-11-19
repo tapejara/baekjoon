e7f4c78168ff8817c9f9dd27130a52c72aabe85f
tapejara <wolfrink444@gmail.com>
2025-11-19 17:01:08 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,21 +1,17 @@
 n, m = map(int,input().split())
 dist_list = [[1e99 for _ in range(n)] for _ in range(n)]
-next_node = [[-1 for _ in range(n)] for _ in range(n)]
 for _ in range(m): 
     a, b = map(int,input().split())
     a -= 1
     b -= 1
     dist_list[a][b] = min(dist_list[a][b], 1)
     dist_list[b][a] = min(dist_list[a][b], 1)
-    next_node[a][b] = b
-    next_node[b][a] = a
 for i in range(n):
     dist_list[i][i] = 0
     for j in range(n):
         for k in range(n):
             if(dist_list[j][k] > dist_list[j][i] + dist_list[i][k]):
                 dist_list[j][k] = dist_list[j][i] + dist_list[i][k]
-                next_node[j][k] = next_node[j][i]
 temp = 1e99
 answer = 0
 for i in range(n):
