e8245980aca6166c58d3d7c482dddeef67badf2a
tapejara <wolfrink444@gmail.com>
2025-12-08 00:25:01 +0900

    [Gold V] Title: 회문 끝말잇기, Time: 1888 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/30641. 회문 끝말잇기/회문 끝말잇기.py
+++ 백준/Gold/30641. 회문 끝말잇기/회문 끝말잇기.py
@@ -1,25 +1,17 @@
 l, u = map(int,input().split())
 answer = 0
 mod = 10 ** 9 + 7
-def function(a):
-    a = bin(a)[3:]
-    b = 26
-    for i in range(len(a)):
-        b = pow(b, 2, mod)
-        if(a[i] == "1"):
-            b = pow(b * 26, 1, mod)
-    return b
 for i in range(l, u + 1):
     if(i < 3):
         answer += 1
     elif(i % 2 == 0):
-        answer += function(((i - 2) // 2)) % mod
+        answer = (answer + pow(26, (i - 2) // 2, mod)) % mod
     else:
-        answer += function(((i - 2) // 2 + 1)) % mod
+        answer = (answer + pow(26, (i - 2) // 2 + 1, mod)) % mod
 answer %= mod
-if((l == 1 and u == 1) or l == 2):
-    print("H")
+if(l != 2 and u != 1):
+    print("A")
     print(answer)
 else:
-    print("A")
+    print("H")
     print(answer)
