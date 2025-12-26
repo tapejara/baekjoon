bd6eaba3ed61cbec89b6c1328a372905910710db
tapejara <wolfrink444@gmail.com>
2025-12-26 17:58:43 +0900

    [Bronze IV] Title: 연대 다음 고대, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34824. 연대 다음 고대/연대 다음 고대.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    a = input()
+    if(a == "yonsei"):
+        print("Yonsei Won!")
+        exit()
+    elif(a == "korea"):
+        print("Yonsei Lost...")
+        exit()
