bc376d482932396d301dfb196f12e2638a448b7e
tapejara <wolfrink444@gmail.com>
2026-01-07 18:59:33 +0900

    [Bronze II] Title: 고고학자 예린, Time: 2904 ms, Memory: 110720 KB -BaekjoonHub

---
+++ 백준/Bronze/20953. 고고학자 예린/고고학자 예린.py
@@ -0,0 +1,7 @@
+t = int(input())
+list1 = [0, 1]
+for i in range(2, 1555):
+    list1.append(list1[-1] + i)
+for i in range(t):
+    a, b = map(int,input().split())
+    print(list1[a + b - 1] * (a + b))
