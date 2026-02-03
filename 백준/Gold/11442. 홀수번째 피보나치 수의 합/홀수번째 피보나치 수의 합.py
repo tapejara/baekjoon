657b7713ce97282578856402a1a13ce34f04e2a0
tapejara <wolfrink444@gmail.com>
2026-02-03 16:39:40 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.py
@@ -0,0 +1,26 @@
+mod = 10 ** 9 + 7
+def function1(arr1, arr2):
+    global mod
+    arr0 = [[0] * 2 for _ in range(2)]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                arr0[i][j] = (arr0[i][j] + (arr1[i][k] * arr2[k][j])) % mod
+    return arr0
+def function2(a, arr1):
+    if(a == 1):
+        return arr1
+    elif(a % 2 == 0):
+        b = function2(a // 2, arr1)
+        c = function1(b, b)
+        return c
+    elif(a % 2 == 1):
+        b = function2((a - 1) // 2, arr1)
+        c1 = function1(b, b)
+        c2 = function1(c1, arr1)
+        return c2
+n = int(input())
+if(n % 2 == 0):
+    print(function2(n, [[1, 1], [1, 0]])[0][1])
+else:
+    print(function2(n + 1, [[1, 1], [1, 0]])[0][1])
