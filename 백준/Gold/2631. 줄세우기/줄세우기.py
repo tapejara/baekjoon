0b87dbd5100bb8c8d66b6694c635e90629375c99
tapejara <wolfrink444@gmail.com>
2026-02-09 19:22:23 +0900

    [Gold IV] Title: 줄세우기, Time: 40 ms, Memory: 34476 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.py
@@ -0,0 +1,14 @@
+from bisect import bisect_left
+n = int(input())
+a = [0]
+for _ in range(n):
+    a.append(int(input()))
+d = [0]
+x = [0]
+for i in range(1, n + 1):
+    if(a[i] > x[-1]):
+        x.append(a[i])
+        d.append(len(d))
+    elif(a[i] < x[-1]):
+        x[bisect_left(x, a[i])] = a[i]
+print(n - d[-1])
