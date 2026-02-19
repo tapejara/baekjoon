f31ff9fc8b68c44377316e5604f6dd1ffe50ac7f
tapejara <wolfrink444@gmail.com>
2026-02-19 14:58:09 +0900

    [Gold II] Title: 반도체 설계, Time: 60 ms, Memory: 38132 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,12 @@
+from bisect import bisect_left
+n = int(input())
+a = [0] + list(map(int,input().split()))
+d = [0]
+x = [0]
+for i in range(1, n + 1):
+    if(a[i] > x[-1]):
+        x.append(a[i])
+        d.append(len(d))
+    elif(a[i] < x[-1]):
+        x[bisect_left(x, a[i])] = a[i]
+print(d[-1])
