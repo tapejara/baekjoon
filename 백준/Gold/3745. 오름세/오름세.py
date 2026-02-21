1f7e820500a495847b54d8b563eb522f3a0a0fae
tapejara <wolfrink444@gmail.com>
2026-02-21 17:23:37 +0900

    [Gold II] Title: 오름세, Time: 100 ms, Memory: 44728 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+while True:
+    try:
+        n = int(input())
+        a = [0] + list(map(int,input().split()))
+        d = [0]
+        x = [0]
+        for i in range(1, n + 1):
+            if(a[i] > x[-1]):
+                x.append(a[i])
+                d.append(len(d))
+            elif(a[i] < x[-1]):
+                x[bisect_left(x, a[i])] = a[i]
+        print(d[-1])
+    except:
+        exit()
